0efb69131477acff65bd10a5c7be09c989796aa6
n-arms <75282394+n-arms@users.noreply.github.com>
2022-11-19 15:10:51 -0500

    Let the user enter their own drivetrain code

--- src/field.ts
+++ src/field.ts
@@ -2,8 +2,7 @@ import { Point } from "./point.js"
 import { Robot } from "./robot.js"
 
 interface Controller {
-    left: () => number;
-    right: () => number;
+    speeds: () => { left: number, right: number }
 }
 
 export class FixedController {
@@ -15,13 +14,71 @@ export class FixedController {
         this.rightConstant = right;
     }
 
-    left(): number {
-        return this.leftConstant
+    speeds(): { left: number, right: number} {
+        return { left: this.leftConstant, right: this.rightConstant };
+    }
+}
+
+export class CodeController {
+    updated: boolean;
+    codeSource: HTMLTextAreaElement;
+    errorField: HTMLTextAreaElement;
+    code: () => { left: number, right: number };
+    rawCode: string;
+
+    constructor(codeSource: HTMLTextAreaElement, errorField: HTMLTextAreaElement) {
+        this.codeSource = codeSource;
+        this.errorField = errorField;
+        this.updated = true;
+        this.rawCode = codeSource.value;
+        this.code = compileControllerCode(this.rawCode, errorField);
     }
 
-    right(): number {
-        return this.rightConstant
+    speeds(): { left: number, right: number } {
+        if (!this.updated) {
+            this.rawCode = this.codeSource.value;
+            this.code = compileControllerCode(this.rawCode, this.errorField);
+        }
+        this.updated = true;
+        return this.code();
     }
+}
+
+const explainCode = `
+Try writing a main function that returns an array containg the speed of the left and right motors. For example:
+
+function main() {
+    return [10, 10];
+}`;
+
+function compileControllerCode(code: string, errorField: HTMLTextAreaElement): () => { left: number, right: number } {
+    try {
+        const fullCode = "(() => {\n" + code + "\nreturn main})"
+        const compiled = eval(fullCode)()
+        if (typeof compiled !== "function") {
+            errorField.value = `Error while compiling code:
+main is not a function
+` + explainCode;
+        } else {
+
+            errorField.value = "";
+            return () => {
+                try {
+                    let [left, right] = compiled();
+                    return { left, right };
+                } catch (e) {
+                    errorField.value = `Error while running controller code:
+${e}`;
+                    return { left: 0, right: 0 };
+                }
+            }
+        }
+    } catch (e) {
+        errorField.value = `Error while compiling code:
+${e}
+` + explainCode;
+    }
+    return () => ({ left: 0, right: 0 });
 }
 
 export class Field {
@@ -36,6 +93,24 @@ export class Field {
     }
 
     tick(seconds: number) {
-        this.robot.tick(this.robotController.left(), this.robotController.right(), seconds);
+        const { left, right } = this.robotController.speeds();
+        this.robot.tick(left, right, seconds);
+
+        if (this.robot.position.x > this.dimensions.x / 2 * 100) {
+            console.log(`collision: ${this.robot.position.x}, ${this.robot.position.y} is out of bounds (x too big)`)
+            this.robot.position.x = this.dimensions.x / 2 * 100;
+        }
+        if (this.robot.position.y > this.dimensions.y / 2 * 100) {
+            console.log(`collision: ${this.robot.position.x}, ${this.robot.position.y} is out of bounds (y too big)`)
+            this.robot.position.y = this.dimensions.y / 2 * 100;
+        }
+        if (-this.robot.position.x > this.dimensions.x / 2 * 100) { 
+            console.log(`collision: ${this.robot.position.x}, ${this.robot.position.y} is out of bounds (x too small)`)
+            this.robot.position.x = -this.dimensions.x / 2 * 100;
+        }
+        if (-this.robot.position.y > this.dimensions.y / 2 * 100) {
+            console.log(`collision: ${this.robot.position.x}, ${this.robot.position.y} is out of bounds (y too small)`)
+            this.robot.position.y = -this.dimensions.y / 2 * 100;
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
 import { renderField, robotCoords } from "./render.js"
-import { Field, FixedController } from "./field.js"
+import { Field, FixedController, CodeController } from "./field.js"
 import { Point } from "./point.js"
 
 const canvas = document.getElementById("field-canvas");
+const codeArea = document.getElementById("code-text-area");
+const updateCodeButton = document.getElementById("update-code-button");
+const codeErrorArea = document.getElementById("code-error-text-area");
 
-if (canvas instanceof HTMLCanvasElement) {
+if (!(canvas instanceof HTMLCanvasElement)) {
+    throw new Error(`expected canvas to be an HTMLCanvasElement, found a ${typeof canvas} instead`);
+} else if (!(codeArea instanceof HTMLTextAreaElement)) {
+    throw new Error(`expected code area to be an HTMLTextAreaElement, found a ${typeof codeArea} instead`);
+} else if (!(updateCodeButton instanceof HTMLButtonElement)) {
+    throw new Error(`expected code update button to be an HTMLButtonElement, found a ${typeof updateCodeButton} instead`);
+} else if (!(codeErrorArea instanceof HTMLTextAreaElement)) {
+    throw new Error(`expected code error area to be an HTMLTextAreaElement, found a ${typeof codeErrorArea} instead`);
+} else {
     const dims = new Point(20.0, 20.0);
-    const field = new Field(dims, new FixedController(10, 5));
+    const controller = new CodeController(codeArea, codeErrorArea);
+    const field = new Field(dims, controller);
+
+    updateCodeButton.addEventListener("click", () => controller.updated = false);
+
 
     let last = Date.now();
 
@@ -22,6 +37,4 @@ if (canvas instanceof HTMLCanvasElement) {
     }
 
     window.requestAnimationFrame(render);
-} else {
-    throw new Error(`expected canvas to be an HTMLCanvasElement, found a ${typeof canvas} instead`);
 }
